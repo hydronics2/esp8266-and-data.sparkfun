33b10dd04be647d0ce68e24737cca7db52bc80eb
hydronics2 <hydronics@gmail.com>
2016-09-15 12:35:59 -0700

    sending a GET to data.sparkfun.com
    NodeMcu esp8266

---
+++ sparkfun_data_export.ino
@@ -0,0 +1,89 @@
+/*
+ *  This sketch sends data via HTTP GET requests to data.sparkfun.com service.
+ *
+ *  You need to get streamId and privateKey at data.sparkfun.com and paste them
+ *  below. Or just customize this script to talk to other HTTP servers.
+ *
+ */
+
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "xxxxxxx";
+const char* password = "xxxxx";
+
+const char* host = "data.sparkfun.com";
+const char* publicKey   = "XG3a7JLA2xCox1DajOr3";
+const char* privateKey = "xxxxxxxxxxxxxxx";
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+int value = 30;
+
+void loop() {
+  delay(5000);
+  ++value;
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+  
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  Serial.println(F("Connected"));
+
+//  Post the data! Request should look a little something like:
+//  GET /input/publicKey?private_key=privateKey&light=1024&switch=0&time=5201 HTTP/1.1\n
+//  Host: data.sparkfun.com\n
+//  Connection: close\n
+//  \n
+     
+  client.print("GET /input/");
+  client.print(publicKey);
+  client.print("?private_key=");
+  client.print(privateKey);
+  client.print("&");
+  client.print("gs");
+  client.print("=");
+  client.print(value);
+
+  client.println(" HTTP/1.1");
+  client.print("Host: ");
+  client.println("data.sparkfun.com");
+  client.println("Connection: close");
+  client.println();
+  
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+    }
+  }
+}
